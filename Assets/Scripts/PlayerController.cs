82e2bc2b33acba21d4404ba28283ab85db7812e7
RinaKovri <ekaterina.romanna@gmail.com>
2022-04-11 14:09:54 +0300

    Player animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float horizontalInput;
-    public float speed = 10.0f;
+    public float speed;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
+        float horizontalInput = Input.GetAxis("Horizontal");
+
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        
     }
 }
+
