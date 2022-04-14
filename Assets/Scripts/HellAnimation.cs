552c13391e4d8c55f7096646e896f36720edf943
GolyshMariia <g.mv@mail.ru>
2022-04-14 14:01:20 +0300

    ScriptEnemy

--- Assets/Scripts/HellAnimation.cs
+++ Assets/Scripts/HellAnimation.cs
@@ -26,13 +26,6 @@ public class HellAnimation : MonoBehaviour
 
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            Destroy(gameObject);
-            anim.SetTrigger("death");
-        }
-    }
+    
 }
 
