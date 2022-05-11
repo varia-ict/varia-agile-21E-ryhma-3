7cb2441f1ff670519c645651daab6930ce25774b
Harles <harles.asu@gmail.com>
2022-05-12 01:55:57 +0300

    Complete rework of the camera system, plus much more. Check Discord for changelog

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -10,7 +10,7 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
     }
 
 }
